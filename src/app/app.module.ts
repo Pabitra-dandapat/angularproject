c330a9f0e97f5926971c84e84887707120d94651
Pabitra-dandapat <67838134+Pabitra-dandapat@users.noreply.github.com>
2021-02-22 20:39:45 +0530

    My first host

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import{FormsModule,ReactiveFormsModule} from '@angular/forms';
+import{HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  
+ }
